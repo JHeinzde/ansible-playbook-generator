ec4a87c43af9d00b0f09a41ea1e16b9b9df8e407
jonathan <Jonathan.Heinz@t-systems.com>
2020-05-19 11:55:41 +0200

    Reverting change from merge

--- playbook-minimizer/src/playbook_minimizer.py
+++ playbook-minimizer/src/playbook_minimizer.py
@@ -25,6 +25,6 @@ class PlaybookMinimizer:
                 if role['role'] in self.changed_roles or role['role'] in roles_to_always_include:
                     new_roles.append(role)
             playbook[counter]['roles'] = new_roles
-            counter += counter
+            counter += 1
 
         self.output_file.write_to_file(yaml.safe_dump(playbook))
